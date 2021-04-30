1ebc4b6340e96694d5e2b61c4f7188e69be1859e
Lokesh B M <lokeshbandi2021@gmail.com>
2021-04-30 10:22:44 +0530

    read sms module completed

--- remote_access_j/app/src/main/java/com/example/remote_access_j/MainActivity.java
+++ remote_access_j/app/src/main/java/com/example/remote_access_j/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.remote_access_j;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //to check if the required permissions are enabled or not
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.RECEIVE_SMS)
+                != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.RECEIVE_SMS},1000);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==1000){
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+            else Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+        }
     }
 }
